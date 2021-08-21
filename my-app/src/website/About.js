24b4cd7711ed3b568af785d6db917db49375e95a
danielle-fong <daniellefong13@gmail.com>
2021-08-21 12:53:42 -0400

    commit on website

---
+++ my-app/src/website/About.js
@@ -0,0 +1,12 @@
+import "./About.css";
+import { Link } from "react-router-dom";
+
+function About(){
+	return (
+		<>
+			<div className = "header">testing block</div>
+		</>
+	);
+};
+
+export default About;
