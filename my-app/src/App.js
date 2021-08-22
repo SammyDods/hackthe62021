e0272742debe18de42654222fa833469d82522d5
danielle-fong <daniellefong13@gmail.com>
2021-08-22 05:34:21 -0400

    commit on website

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import Players from "./website/Players";
 import two from "./website/two";
 import three from "./website/three";
 import four from "./website/four";
+import score from "./website/score";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -16,6 +17,7 @@ const App = () => {
 					<Route path="/2players" exact component={two} />
 					<Route path="/3players" exact component={three} />
 					<Route path="/4players" exact component={four} />
+					<Route path="/scoreboard" exact component={score} />
 				</Switch>
 			</Router>
 		</>
