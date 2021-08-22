edbc1d5566530c22d5666854784c77fb830086bd
danielle-fong <daniellefong13@gmail.com>
2021-08-22 01:15:05 -0400

    commit on player page

--- my-app/src/website/players/two.js
+++ my-app/src/website/players/two.js
@@ -1,1 +1,13 @@
-import ./two.css;
+import "./two.css";
+
+function two(){
+	return (
+		<>
+			<div className = "two-players">
+				<div className = "test">hello</div>
+			</div>
+		</>
+	);
+};
+
+export default two;
