2241a62f12d4fe3746f588ac1753d29569535137
danielle-fong <daniellefong13@gmail.com>
2021-08-22 05:34:14 -0400

    commit on website

---
+++ my-app/src/website/score.js
@@ -0,0 +1,14 @@
+import "./score.css";
+import { Link } from "react-router-dom";
+import title from "./title.png";
+
+
+function Score(){
+	return (
+		<>
+			<div className = "scores">SCOREBOARD</div>
+		</>
+	);
+};
+
+export default Score;
